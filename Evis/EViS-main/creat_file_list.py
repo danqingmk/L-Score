6e6c50b283951b156d37b04e0e056929ca6f2c9d
danqingmk <48748637+danqingmk@users.noreply.github.com>
2024-03-29 11:15:55 +0800

    Delete Evis/EViS-main/creat_file_list.py

--- Evis/EViS-main/creat_file_list.py
+++
@@ -1,6 +0,0 @@
-import os
-for i in range(1,201):
-    file_n='/data/Mukuo/A1AR/ucsf_redock/active_better_result/'+'active'+str(i)+'_out_ligand_1'+'.mol2'
-    out_n='/data/Mukuo/A1AR/ucsf_redock/active_decoy_mol2_for_plscore/'+'active'+str(i)+'_out_ligand_1'+'.mol2'
-    comand='scp -r %s %s' % (file_n,out_n)
-    os.system(comand)
