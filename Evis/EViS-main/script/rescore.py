99827361416d299119e60d3c8a01efc57c8862d0
danqingmk <48748637+danqingmk@users.noreply.github.com>
2024-03-29 11:31:28 +0800

    Delete Evis/EViS-main directory

--- Evis/EViS-main/script/rescore.py
+++
@@ -1,3 +0,0 @@
-# we need to create new dir which contains mol2 files ,and the dir need to set in '/user_input/results_docking'
-# we prepare 2 file to new dir a. pocket_topN.info b. pocket_topN.mol2
-# c. POC
